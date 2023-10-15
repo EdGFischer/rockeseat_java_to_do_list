563b8ffe278ec09b291f8d29bf6be32aa515fa65
EdGFischer <eduardogfischer@gmail.com>
2023-10-15 18:19:40 -0300

    First Commit

---
+++ src/main/java/br/com/eduardogfischer/todolist/user/IUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.eduardogfischer.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface IUserRepository extends JpaRepository<UserModel,  UUID>{
+     
+    UserModel findByUsername(String username);;
+}
